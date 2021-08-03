d07cd60fbf4878d43aaa50139dbd087230ca686b
SoLeFlem <88395713+SoLeFlem@users.noreply.github.com>
2021-08-03 19:07:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function poulax()
+{
+	var poulax = document.getElementById("poulax").value;
+	if (poulax == "oui")
+	{
+		alert("Ok, Ok");
+	}
+	if (poulax == "non")
+	{
+		alert("Allez casse toi");
+	}
+	if (poulax == "mms")
+	{
+		alert("ldo");
+	}
+	if (poulax == "667")
+	{
+		alert("ekipafond");
+	}
+
+}
